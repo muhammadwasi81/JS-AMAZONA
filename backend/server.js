613d9ab9c273ef309200d13961c88fb1a969f240
muhammad wasi81 <wasiarain81@gmail.com>
2021-03-22 02:05:30 +0500

    initialize

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const express= require("express");
-const data = reuire('./data.js')
+import express from "express";
+import cors from "cors";
+import data from "./data.js";
+
 const app = express();
+app.use(cors());
 
-app.get('/api/products' , (req, res) => {
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 
+app.listen(5000, () => {
+  console.log("serve at http://localhost:5000");
 });
