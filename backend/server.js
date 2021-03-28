2a685406865c3853776a778bd53b710270a2ef3c
muhammad wasi81 <wasiarain81@gmail.com>
2021-03-28 21:08:02 +0500

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 import express from "express";
 import cors from "cors";
 import data from "./data";
+import mongoose from 'mongoose';
+import config from "./config";
+import userRouter from "./routers/userRouter";
 
+
+
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 const app = express();
 app.use(cors());
+
+app.use("/api/users", userRouter);
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
